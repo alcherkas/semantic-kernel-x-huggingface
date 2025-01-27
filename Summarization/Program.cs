f5b4108793d1ef0cfb1734220715434e19746b2f
Aleksandr Cherkas <cherkasalexandr@gmail.com>
2025-01-27 19:58:03 +0200

    Add Summarization task

---
+++ Summarization/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.TextGeneration;
+#pragma warning disable SKEXP0010
+
+var model = "llama-3.2-1b-instruct";
+var uri = new Uri("http://127.0.0.1:1234/v1");
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(model, uri, "API_KEY")
+    .Build();
+
+var textGeneration = kernel.GetRequiredService<ITextGenerationService>();
+var result = await textGeneration.GetTextContentsAsync("Summarize: Paris is the capital and most populous city of France, with\n          an estimated population of 2,175,601 residents as of 2018,\n          in an area of more than 105 square kilometres (41 square\n          miles). The City of Paris is the centre and seat of\n          government of the region and province of Île-de-France, or\n          Paris Region, which has an estimated population of\n          12,174,880, or about 18 percent of the population of France\n          as of 2017.");
+
+Console.WriteLine(result[0].Text);
